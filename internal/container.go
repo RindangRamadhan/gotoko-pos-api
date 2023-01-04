977529d4cfa712c0ce75ef9ef92d4fd680f94065
Rindang Ramadhan <rindang.ramadhan@efishery.com>
2023-01-04 23:32:02 +0700

    chore(pkg) : - add new csutome error mapping

--- internal/container.go
+++ internal/container.go
@@ -3,11 +3,23 @@ package internal
 import (
 	"gotoko-pos-api/internal/pkg"
 	"gotoko-pos-api/internal/pkg/env"
+	"gotoko-pos-api/internal/repositories/cashier"
+	"gotoko-pos-api/internal/usecases/cashier/createcashier"
+	"gotoko-pos-api/internal/usecases/cashier/deletecashier"
+	"gotoko-pos-api/internal/usecases/cashier/getcashier"
+	"gotoko-pos-api/internal/usecases/cashier/getcashierdetail"
+	"gotoko-pos-api/internal/usecases/cashier/updatecashier"
 	"gotoko-pos-api/internal/usecases/health"
 )
 
 type Container struct {
 	HealthCheckUsecase health.Inport
+
+	GetCashierUsecase       getcashier.Inport
+	GetCashierDetailUsecase getcashierdetail.Inport
+	CreateCashierUsecase    createcashier.Inport
+	UpdateCashierUsecase    updatecashier.Inport
+	DeleteCashierUsecase    deletecashier.Inport
 }
 
 func NewContainer() *Container {
@@ -23,7 +35,16 @@ func NewContainer() *Container {
 		MaxLifeTimeConn: env.Get().DBMaxLifeTimeConn,
 	})
 
+	// Repo for query related
+	cashierRepo := cashier.NewRepo(dbClient)
+
 	return &Container{
 		HealthCheckUsecase: health.NewUsecase(dbClient),
+
+		GetCashierUsecase:       getcashier.NewUsecase(cashierRepo),
+		GetCashierDetailUsecase: getcashierdetail.NewUsecase(cashierRepo),
+		CreateCashierUsecase:    createcashier.NewUsecase(cashierRepo),
+		UpdateCashierUsecase:    updatecashier.NewUsecase(cashierRepo),
+		DeleteCashierUsecase:    deletecashier.NewUsecase(cashierRepo),
 	}
 }
--- internal/pkg/errors.go
+++ internal/pkg/errors.go
@@ -1,8 +1,66 @@
 package pkg
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	commonerror "gotoko-pos-api/common/errors"
+)
+
+const (
+	ErrCodeDefault = "SERVER-00"
+
+	ErrCodeNotFound   = "DATA-01"
+	ErrCodeFatalQuery = "QUERY-01"
+	ErrCodePayload    = "PAYLOAD-01"
+)
 
 var (
-	ErrFatalQuery   = errors.New("fatal query error")
-	ErrDataNotFound = errors.New("data not found")
+	ErrFatalQuery = errors.New("fatal query error")
+	ErrJSONParse  = errors.New("an error occurred in the input parameter")
+
+	ErrCashierNotFound = errors.New("cashier not found")
 )
+
+func FormatErrorToCommonError(err error) *commonerror.Err {
+	// FATAL QUERY
+	ErrInternalFatalQueries := []error{
+		ErrFatalQuery,
+	}
+
+	if ErrorContains(ErrInternalFatalQueries, err) {
+		return commonerror.NewErr(http.StatusInternalServerError, ErrCodeFatalQuery, err.Error())
+	}
+
+	// BAD REQUEST
+	ErrBadRequest := []error{
+		ErrJSONParse,
+	}
+
+	if ErrorContains(ErrBadRequest, err) {
+		return commonerror.NewErr(http.StatusBadRequest, ErrCodePayload, err.Error())
+	}
+
+	// NOT FOUND
+	ErrDataNotFound := []error{
+		ErrCashierNotFound,
+	}
+
+	if ErrorContains(ErrDataNotFound, err) {
+		return commonerror.NewErr(http.StatusNotFound, ErrCodeNotFound, err.Error())
+	}
+
+	// DEFAULT
+	return commonerror.NewErr(http.StatusInternalServerError, ErrCodeDefault, fmt.Sprintf("unexpected error from server : %v", err))
+}
+
+func ErrorContains(errors []error, err error) bool {
+	for _, e := range errors {
+		if e == err {
+			return true
+		}
+	}
+
+	return false
+}
